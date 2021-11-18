14979e1e0d5c0d2d71bef0a1505ccdb1394d68bf
aiman-al-masoud <luxlunarislabs@gmail.com>
2021-11-18 18:34:59 +0100

    some more work on authentication

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 # Always remember to pull before pushing!
 
-
+# imports
 import pandas as pd
-
-
 from flask import Flask, render_template, request
 
+# constants
+PASSWORDS_TABLE_PATH = "./data/passwords_table.csv"
 
+
+# start the app
 app = Flask(__name__)
-
 app.config["DEBUG"] = True
 
 
 
-HASHED_PASSWORDS_TABLE = "./data/passwords_table.pickle"
 
 
 
@@ -29,12 +29,21 @@ def on_login():
 @app.route("/post_login", methods = ["POST", "GET"])
 def on_post_login():
 
+    # load passwords table
+    df = load_passwords_table()
+
+    # get input data from form
     username = request.form["username"]
     password_attempt = request.form["password"]
 
-    df = load_passwords_table()
 
-    if df[df.username==username].hashed_password.to_list()[0] == hash(password_attempt):
+    # check if username exists
+    if not user_exists(username):
+        return "You don't have an account yet!"
+
+
+    # compare entered password's hash to actual password's hash
+    if df[df.username==username].hashed_password.to_list()[0] == hash_password(password_attempt):
         return "It's you!"
 
     return "You're not who you say you are!"    
@@ -49,42 +58,68 @@ def on_signup():
 @app.route("/post_signup", methods = ["POST", "GET"])
 def on_post_signup():
 
+    # load passwords table
+    df = load_passwords_table()
+
+    # get input data from form
     username = request.form["username"]
     password = request.form["password"]
     password_confirm = request.form["password_confirm"]
 
+    # check if username is already taken
+    if user_exists(username):
+        return f'Username {username} is already taken!'
+
+
+    # check if new password was confirmed correctly
     if password != password_confirm:
         return "Looks like a typo! Passwords don't match! Signup aborted."
 
-    df = load_passwords_table()
-
-    row = pd.DataFrame([(username, hash(password))], columns=["username", "hashed_password"]) 
-
+    # add the user and hashed password to the passwords table    
+    row = pd.DataFrame([(username, hash_password(password))], columns=["username", "hashed_password"]) 
     df = df.append(row)
-
     store_passwords_table(df)
 
-    return "Welcome"+username+"!"
+    # welcome new user
+    return f'Welcome {username}!'
 
 
-
-# username : hashed_password
 def load_passwords_table():
+
+    """
+    Load the passwords table.
+    """
     try:
-        return pd.read_pickle(HASHED_PASSWORDS_TABLE)
+        # read the table from a csv
+        return pd.read_csv(PASSWORDS_TABLE_PATH)
     except:
+        # initialize a new passwords table if file not found
         return pd.DataFrame([], columns=["username", "hashed_password"])
+    
 
 
 def store_passwords_table(df):
-    df.to_pickle(HASHED_PASSWORDS_TABLE)
+    """
+    Store the passwords table.
+    Arguments: passwords dataframe.
+    """
+    df.to_csv(PASSWORDS_TABLE_PATH, index=False)
 
 
 
+def user_exists(username):
+    """
+    Checks if a username is taken.
+    """
+
+    df = load_passwords_table()
+    return (df.username==username).sum() != 0 
 
 
-
-
+# TODO: don't use default hashing function, but not this stupid one!
+def hash_password(password):
+    l = [ord(x) for x in list(password)]
+    return sum(l)
 
 
 
